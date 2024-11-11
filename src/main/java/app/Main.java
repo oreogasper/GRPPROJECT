daf9bac325fdd3785f14591614c9444bbc3e6013
jashvirr <jashr212@gmail.com>
2024-11-11 18:21:36 -0500

    added cancel usecase to main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -21,6 +21,7 @@ public class Main {
                                             .addLoginUseCase()
                                             .addChangePasswordUseCase()
                                             .addLogoutUseCase()
+                .addCancelUseCase()
                                             .build();
 
         application.pack();
