c0f071064464326349d455d2c98b5a91b05d0145
jashvirr <jashr212@gmail.com>
2024-11-11 19:26:14 -0500

    the interactor for the cancel use case

---
+++ src/main/java/use_case/cancel/CancelInteractor.java
@@ -0,0 +1,22 @@
+package use_case.cancel;
+
+
+/**
+ * The Logout Interactor.
+ */
+public class CancelInteractor {
+    private final CancelOutputBoundary logoutPresenter;
+
+    public CancelInteractor(CancelOutputBoundary logoutOutputBoundary) {
+        this.logoutPresenter = logoutOutputBoundary;
+    }
+
+    @Override
+    public void execute() {
+        // * instantiate the `CancelOutputData`, which DOESNT need to contain the username
+        final CancelOutputData logoutData = new CancelOutputData(false);
+
+        // * tell the presenter to prepare a success view.
+        logoutPresenter.prepareSuccessView(logoutData);
+    }
+}
