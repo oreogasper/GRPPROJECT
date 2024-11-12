4f6583097a4acd3fc2ac236e2cf149ea5335b3ea
jashvirr <jashr212@gmail.com>
2024-11-11 19:22:59 -0500

    the input boundary interface for cancelling

---
+++ src/main/java/use_case/cancel/CancelInputBoundary.java
@@ -0,0 +1,12 @@
+package use_case.cancel;
+
+/**
+ * Input Boundary for actions which are related to cancelling.
+ */
+public interface CancelInputBoundary {
+
+    /**
+     * Executes the Cancel use case.
+     */
+    void execute();
+}
