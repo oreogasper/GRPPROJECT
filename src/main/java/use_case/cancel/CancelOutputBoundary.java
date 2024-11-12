4d34ae280d7bf4e692bfc67fc597f440a50c9e32
jashvirr <jashr212@gmail.com>
2024-11-11 19:23:24 -0500

    the output boundary interface for cancelling

---
+++ src/main/java/use_case/cancel/CancelOutputBoundary.java
@@ -0,0 +1,19 @@
+package use_case.cancel;
+
+
+/**
+ * The output boundary for the Cancel Use Case.
+ */
+public interface CancelOutputBoundary {
+    /**
+     * Prepares the success view for the Cancel Use Case.
+     * @param outputData the output data
+     */
+    void prepareSuccessView(CancelOutputData outputData);
+
+    /**
+     * Prepares the failure view for the Cancel Use Case.
+     * @param errorMessage the explanation of the failure
+     */
+    void prepareFailView(String errorMessage);
+}
