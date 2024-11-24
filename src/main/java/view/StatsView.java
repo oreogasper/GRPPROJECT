e9d6a74d78759ca6bf707de1da97a72a3c45caa3
oreogasper <1chowemm@gmail.com>
2024-11-24 01:05:31 -0500

    Stats table now dynamically updates balance

--- src/main/java/view/StatsView.java
+++ src/main/java/view/StatsView.java
@@ -1,47 +1,54 @@
 package view;
 
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.table.DefaultTableModel;
 
-import interface_adapter.statistics.ChangePasswordController;
+import entity.User;
 import interface_adapter.logout.LogoutController;
-
+import interface_adapter.statistics.ChangePasswordController;
 import interface_adapter.statistics.StatisticsController;
 import interface_adapter.statistics.StatisticsState;
 import interface_adapter.statistics.StatisticsViewModel;
-
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.beans.PropertyChangeListener;
 
 /**
  * The View for the Statistics Use Case.
  */
 public class StatsView extends JPanel implements PropertyChangeListener {
+    private static final String INITIAL_VALUE = "0";
+    private static final Integer TABLE_WIDTH = 200;
+    private static final Integer TABLE_HEIGHT = 125;
+    private static final Integer ROW_THREE = 3;
+    private static final Integer ROW_FOUR = 4;
+    private static final Integer ROW_FIVE = 5;
+
     private final String viewName = "stats";
     private final StatisticsViewModel statisticsViewModel;
     private ChangePasswordController changePasswordController;
-
     private StatisticsController statisticsController;
 
-    private final JLabel username;
+    private final DefaultTableModel tableModel;
+    private final JTable table;
     private final JButton logOut;
     private final JTextField passwordInputField = new JTextField(15);
     private final JButton changePassword;
-
     private final JButton cancel;
     private final JButton leaderboard;
-    JTable j;
     private LogoutController logoutController;
 
     public StatsView(StatisticsViewModel statisticsViewModel) {
@@ -54,13 +61,9 @@ public class StatsView extends JPanel implements PropertyChangeListener {
         final JPanel button = new JPanel();
         leaderboard = new JButton(StatisticsViewModel.TO_LEADERBOARD_BUTTON_LABEL);
         button.add(leaderboard);
-        // TODO:
+
         final LabelTextPanel passwordInfo = new LabelTextPanel(
                 new JLabel("Change Password To:"), passwordInputField);
-
-        username = new JLabel();
-        final StatsTextPanel userInfo = new StatsTextPanel(
-                new JLabel("Currently logged in: "), username);
 
         final JPanel buttons = new JPanel();
         logOut = new JButton("Log Out");
@@ -69,27 +72,27 @@ public class StatsView extends JPanel implements PropertyChangeListener {
         changePassword = new JButton("Change Password");
         buttons.add(changePassword);
 
-        //TODO:
         cancel = new JButton(StatisticsViewModel.RETURN_MENU_BUTTON_LABEL);
         buttons.add(cancel);
 
-        // TEMPORARY BUTTONS
-        final JTable info = new JTable();
-        String[][] data = {
-                {"Tokens", "0"},
-                {"Wins", "10"},
-                {"Losses", "20"},
-                {"Win percentage", "30"},
-                {"Games", "40"},
+        // Initializing table
+        final String[] columnNames = {"Statistic", "Value"};
+        final String[][] initialData = {
+                {"Currently logged in", "username"},
+                {"Balance", INITIAL_VALUE},
+                {"Wins", INITIAL_VALUE},
+                {"Losses", INITIAL_VALUE},
+                {"Win percentage", INITIAL_VALUE},
+                {"Games", INITIAL_VALUE},
         };
+        tableModel = new DefaultTableModel(initialData, columnNames) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        table = new JTable(tableModel);
 
-        // Column Names
-        final String[] columnNames = {"Name", "Roll Number"};
-
-        // Initializing the JTable
-        j = new JTable(data, columnNames);
-        j.setBounds(0, 10, getWidth(), getHeight());
-        // TODO:
         passwordInputField.getDocument().addDocumentListener(new DocumentListener() {
 
             private void documentListenerHelper() {
@@ -115,27 +118,21 @@ public class StatsView extends JPanel implements PropertyChangeListener {
         });
 
         changePassword.addActionListener(
-                // This creates an anonymous subclass of ActionListener and instantiates it.
                 evt -> {
                     if (evt.getSource().equals(changePassword)) {
                         final StatisticsState currentState = statisticsViewModel.getState();
-
                         this.changePasswordController.execute(
                                 currentState.getUsername(),
                                 currentState.getPassword(),
-                                currentState.getUser().getInfo()
-                        );
+                                currentState.getUser().getInfo());
                     }
                 }
         );
 
         logOut.addActionListener(
-                // This creates an anonymous subclass of ActionListener and instantiates it.
                 evt -> {
                     if (evt.getSource().equals(logOut)) {
-                        StatisticsState currentState = statisticsViewModel.getState();
-                        // TODO:
-                        System.out.println(currentState);
+                        final StatisticsState currentState = statisticsViewModel.getState();
                         this.logoutController.execute(currentState.getUsername());
                     }
                 }
@@ -161,19 +158,29 @@ public class StatsView extends JPanel implements PropertyChangeListener {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         this.add(title);
-        this.add(userInfo);
 
         this.add(button);
         this.add(buttons);
         this.add(passwordInfo);
-        this.add(j);
+        final JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setPreferredSize(new Dimension(TABLE_WIDTH, TABLE_HEIGHT));
+        this.add(scrollPane);
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         if (evt.getPropertyName().equals("state")) {
             final StatisticsState state = (StatisticsState) evt.getNewValue();
-            username.setText(state.getUsername());
+            final User user = state.getUser();
+
+            tableModel.setValueAt(state.getUsername(), 0, 1);
+            tableModel.setValueAt(String.valueOf(user.getBalance()), 1, 1);
+            tableModel.setValueAt(String.valueOf(user.getWins()), 2, 1);
+            tableModel.setValueAt(String.valueOf(user.getLosses()), ROW_THREE, 1);
+            // TODO: when games is 0, add 1
+            tableModel.setValueAt(String.valueOf(user.getWins() / (user.getGames() + 1)), ROW_FOUR, 1);
+            tableModel.setValueAt(String.valueOf(user.getGames()), ROW_FIVE, 1);
+
         }
 
         else if (evt.getPropertyName().equals("password")) {
@@ -194,6 +201,7 @@ public class StatsView extends JPanel implements PropertyChangeListener {
     public void setStatisticsController(StatisticsController controller) {
         this.statisticsController = controller;
     }
+
     public void setLogoutController(LogoutController logoutController) {
         this.logoutController = logoutController;
     }
