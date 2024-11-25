099c2aa16f5d6773c879616fb4ddf4ddc5e35136
jashvirr <jashr212@gmail.com>
2024-11-25 17:50:19 -0500

    game entity for blackjack game

---
+++ src/main/java/entity/BlackjackGame.java
@@ -0,0 +1,66 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlackjackGame implements Game{
+    private final int minBet = 100;
+    private final int maxBet = 1000;
+    private final String rules = "";
+    private final String gameName = "Blackjack";
+
+    private List<Card> playerCards;
+    private List<Card> dealerCards;
+
+    public BlackjackGame() {
+        playerCards = new ArrayList<Card>();
+        dealerCards = new ArrayList<Card>();
+    }
+
+    @Override
+    public int getMinBet() {
+        return minBet;
+    }
+
+    @Override
+    public int getMaxBet() {
+        return maxBet;
+    }
+
+    @Override
+    public String getRules() {
+        return rules;
+    }
+
+    @Override
+    public String getGameType() {
+        return gameName;
+    }
+
+    private int cardsScore(List<Card> cards) {
+        int score = 0;
+        for (Card card : cards) {
+            score += card.getRank();
+        }
+        return score;
+    }
+
+    public void addPlayerCard(Card card) {
+        playerCards.add(card);
+    }
+
+    public void addDealerCard(Card card) {
+        dealerCards.add(card);
+    }
+
+    public boolean isBust(List<Card> cards) {
+        int totalScore = this.cardsScore(cards);
+        return totalScore > 21;
+    }
+
+    public boolean isWin(List<Card> cards) {
+        int totalScore = this.cardsScore(cards);
+        return totalScore == 21;
+    }
+
+}
