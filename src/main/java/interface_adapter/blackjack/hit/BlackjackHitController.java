5d60a5769360e56f54ef28ec544390d32469b8a7
Jashvir Rathore <jashvir.rathore@mail.utoronto.ca>
2024-11-25 23:17:04 -0500

    controller for hit use case

--- src/main/java/interface_adapter/blackjack/hit/BlackjackHitController.java
+++ src/main/java/interface_adapter/blackjack/hit/BlackjackHitController.java
@@ -1,4 +1,33 @@
 package interface_adapter.blackjack.hit;
 
+import use_case.blackjack.hit.BlackjackHitInputBoundary;
+import use_case.blackjack.hit.BlackjackHitInputData;
+
+/**
+ * Controller for the Blackjack Hit Use Case.
+ */
 public class BlackjackHitController {
+    private final BlackjackHitInputBoundary blackjackHitUseCaseInteractor;
+
+    public BlackjackHitController(BlackjackHitInputBoundary blackjackHitInputBoundary) {
+        this.blackjackHitUseCaseInteractor = blackjackHitInputBoundary;
+    }
+
+    /**
+     * Executes the Blackjack Hit Use Case.
+     */
+    public void execute() {
+
+        final BlackjackHitInputData inputData = new BlackjackHitInputData();
+
+        blackjackHitUseCaseInteractor.execute(inputData);
+    }
+
+    /**
+     * Executes the "switch to GameMenuView" Use Case.
+     */
+    public void switchToGameMenuView() {
+        blackjackHitUseCaseInteractor.switchToGameMenuView();
+    }
+
 }
