6be9f6547ee6a750976d344fec6ce6ec7cc0c767
jashvirr <jashr212@gmail.com>
2024-11-26 15:59:14 -0500

    view model and game state updated with info relevant to game

--- src/main/java/interface_adapter/blackjack/game/BlackjackGameState.java
+++ src/main/java/interface_adapter/blackjack/game/BlackjackGameState.java
@@ -1,7 +1,103 @@
 package interface_adapter.blackjack.game;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The state for the Blackjack Game View Model.
  */
 public class BlackjackGameState {
+    private String playerScore;
+    private String dealerScore;
+    private java.util.List<Image> playerCards;
+    private List<Image> dealerCards;
+
+    private boolean playerCardsDefault;
+    private boolean dealerCardsDefault;
+
+    private String gameState;
+
+    public BlackjackGameState() {
+
+        playerCards = new ArrayList<>();
+        playerCards.add(this.loadCardHiddenImage());
+        playerCards.add(this.loadCardHiddenImage());
+        playerCardsDefault = true;
+
+        dealerCards = new ArrayList<>();
+        dealerCards.add(this.loadCardHiddenImage());
+        dealerCards.add(this.loadCardHiddenImage());
+        dealerCardsDefault = true;
+
+        playerScore = "0";
+        dealerScore = "0";
+
+        gameState = "Player";
+    }
+
+    public void setPlayerScore(int score) {
+        playerScore = String.valueOf(score);
+    }
+
+    public void setDealerScore(int score) {
+        dealerScore = String.valueOf(score);
+    }
+
+    public String getPlayerScore() {
+        return playerScore;
+    }
+
+    public String getDealerScore() {
+        return dealerScore;
+    }
+
+    public void addPlayerCard(Image card) {
+        if (playerCardsDefault) {
+            playerCards.clear();
+            playerCards.add(card);
+        }
+        else {
+            playerCards.add(card);
+        }
+    }
+
+    public void addDealerCard(Image card) {
+        if (dealerCardsDefault) {
+            dealerCards.clear();
+            dealerCards.add(card);
+        }
+        else {
+            dealerCards.add(card);
+        }
+    }
+
+    public List<Image> getPlayerCards() {
+        return playerCards;
+    }
+
+    public List<Image> getDealerCards() {
+        return dealerCards;
+    }
+
+    private Image loadCardHiddenImage() {
+        Image image = null;
+
+        try {
+            final BufferedImage bufferedImage1 = ImageIO.read(new File("images/back-card.png"));
+            image = bufferedImage1.getScaledInstance(
+                    (int) Math.round(bufferedImage1.getWidth() * 0.12),
+                    (int) Math.round(bufferedImage1.getHeight() * 0.12), Image.SCALE_SMOOTH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return image;
+
+    }
+
 }
--- src/main/java/interface_adapter/blackjack/game/BlackjackGameViewModel.java
+++ src/main/java/interface_adapter/blackjack/game/BlackjackGameViewModel.java
@@ -9,8 +9,14 @@ import view.BlackjackGameView;
 public class BlackjackGameViewModel extends ViewModel<BlackjackGameState> {
     public static final String TITLE_LABEL = "Play Blackjack!";
     public static final String PLAYER_HAND_LABEL = "Your Hand:";
+    public static final String DEALER_HAND_LABEL = "Dealer Hand:";
     public static final String HIT_LABEL = "Hit";
     public static final String STAND_LABEL = "Stand";
+    public static final String SCORE_LABEL = "Score:";
+    public static final String PLAYER_TURN_LABEL = "Your Turn";
+    public static final String DEALER_TURN_LABEL = "Dealers Turn";
+    public static final String WIN_LABEL = "You Win!";
+    public static final String LOSE_LABEL = "You Lose!";
 
     public BlackjackGameViewModel() {
         super("Blackjack Game");
@@ -20,4 +26,5 @@ public class BlackjackGameViewModel extends ViewModel<BlackjackGameState> {
     public void addPropertyChangeListener(BlackjackGameView blackjackGameView) {
 
     }
+
 }
