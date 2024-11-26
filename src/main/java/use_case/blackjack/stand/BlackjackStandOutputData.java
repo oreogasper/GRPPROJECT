83880e12c38c20e42fae76ba21af1a016f7d2124
Jashvir Rathore <jashvir.rathore@mail.utoronto.ca>
2024-11-25 23:04:21 -0500

    output data for stand usecase

--- src/main/java/use_case/blackjack/stand/BlackjackStandOutputData.java
+++ src/main/java/use_case/blackjack/stand/BlackjackStandOutputData.java
@@ -4,5 +4,20 @@ package use_case.blackjack.stand;
  * Output Data for the Blackjack Stand Use Case.
  */
 public class BlackjackStandOutputData {
+    private final int playerScore;
+    private final boolean useCaseFailed;
+
+    public BlackjackStandOutputData(int playerScore, boolean useCaseFailed) {
+        this.playerScore = playerScore;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public int getPlayerScore() {
+        return playerScore;
+    }
+
+    public boolean isUseCaseFailed() {
+        return useCaseFailed;
+    }
 
 }
