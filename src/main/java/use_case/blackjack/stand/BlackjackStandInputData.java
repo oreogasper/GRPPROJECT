40c45ea364114846c11a02bfc1e9806fa518de70
jashvirr <jashr212@gmail.com>
2024-11-26 22:15:48 -0500

    added param to input data

--- src/main/java/use_case/blackjack/stand/BlackjackStandInputData.java
+++ src/main/java/use_case/blackjack/stand/BlackjackStandInputData.java
@@ -4,8 +4,10 @@ package use_case.blackjack.stand;
  * Input Data for the Blackjack Stand Use Case.
  */
 public class BlackjackStandInputData {
+    private final boolean isEndPlayerTurn;
 
-    public BlackjackStandInputData() {
+    public BlackjackStandInputData(boolean isEndPlayerTurn) {
+        this.isEndPlayerTurn = isEndPlayerTurn;
 
     }
 
