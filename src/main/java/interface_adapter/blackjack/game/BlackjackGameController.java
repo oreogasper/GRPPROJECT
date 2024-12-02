b31d897f836dd64f1882cdacae673a403ac1ddd1
Jashvir Rathore <jashvir.rathore@mail.utoronto.ca>
2024-12-02 10:16:36 -0500

    facade finished

--- src/main/java/interface_adapter/blackjack/game/BlackjackGameController.java
+++ src/main/java/interface_adapter/blackjack/game/BlackjackGameController.java
@@ -1,23 +1,32 @@
 package interface_adapter.blackjack.game;
 
 
+import entity.User;
 import use_case.blackjack.game.BlackjackGameInputBoundary;
+import use_case.blackjack.game.BlackjackGameInputData;
 
 /**
  * Controller for the Blackjack Game Use Case.
  */
 public class BlackjackGameController {
-    private final BlackjackGameInputBoundary blackjackGameUseCaseInteractor;
+    private final BlackjackGameInputBoundary blackjackGameInputBoundary;
 
     public BlackjackGameController(BlackjackGameInputBoundary blackjackGameInputBoundary) {
-        this.blackjackGameUseCaseInteractor = blackjackGameInputBoundary;
+        this.blackjackGameInputBoundary = blackjackGameInputBoundary;
+    }
+
+    public void execute(String useCaseName, int bet, User user, String turnSate) {
+
+        BlackjackGameInputData inputData = new BlackjackGameInputData(useCaseName, bet, user, turnSate);
+        blackjackGameInputBoundary.execute(inputData);
+
     }
 
     /**
      * Executes the "switch to LoginView" Use Case.
      */
     public void switchToLoginView() {
-        blackjackGameUseCaseInteractor.switchToLoginView();
+        blackjackGameInputBoundary.switchToLoginView();
     }
 
 }
