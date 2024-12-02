325aeff8f3cfed7a0a1e7375fc06f5cb9d0b01e4
Jashvir Rathore <jashvir.rathore@mail.utoronto.ca>
2024-12-02 11:31:37 -0500

    files for betview nice looking

--- src/main/java/interface_adapter/blackjack/bet/BlackjackBetViewModel.java
+++ src/main/java/interface_adapter/blackjack/bet/BlackjackBetViewModel.java
@@ -7,9 +7,14 @@ import interface_adapter.ViewModel;
  */
 public class BlackjackBetViewModel extends ViewModel<BlackjackBetState> {
     public static final String TITLE_LABEL = "PLACE YOUR BET";
-    public static final String BET_LABEL = "Bet amount";
-    public static final String CONTINUE_BUTTON_LABEL = "continue";
-    public static final String BACK_BUTTON_LABEL = "back";
+    public static final String BET_LABEL = "BET AMOUNT:";
+    public static final String CONTINUE_BUTTON_LABEL = "CONTINUE";
+    public static final String BACK_BUTTON_LABEL = "BACK";
+    public static final String FONT_NAME = "Serif";
+    public static final int TITLE_SIZE = 30;
+    public static final int SUBTITLE_SIZE = 20;
+    public static final int WIDTH_DIM = 50;
+    public static final int HEIGHT_DIM = 50;
 
     public BlackjackBetViewModel() {
         super("Blackjack bet");
